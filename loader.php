5234f537244f4863f65d713c2db64a7b062279f7
boonebgorges <boonebgorges@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-05-23 16:00:01 +0000

    Introduces loader.php to make the main plugin class file easier to copy and paste to other themes and plugins
    git-svn-id: http://plugins.svn.wordpress.org/boones-sortable-columns/trunk@388122 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ loader.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+Plugin Name: Boone's Sortable Columns
+Plugin URI: http://github.com/boonebgorges/boones-sortable-columns
+Description: A handy, extensible sortable tables class for WordPress custom post type lists (or other kinds of content)
+Version: 1.0
+Author: Boone B Gorges
+Author URI: http://boonebgorges.com
+Donate link: http://teleogistic.net/donate
+Licence: GPLv3
+*/
+
+/**
+ * The main plugin content is loaded this way so that the plugin header is isolated in its own file.
+ * This makes it easier to copy the class file wholesale to your own plugin or theme.
+ */
+require_once( dirname( __FILE__ ) . '/boones-sortable-columns.php' );
+
+
+?>
